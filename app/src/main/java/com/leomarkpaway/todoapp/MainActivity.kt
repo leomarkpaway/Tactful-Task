f9a297192807b83f6293401bf16e2e136c08b36b
leomark paway <leomarkpaway@gmail.com>
2023-11-11 10:19:48 +0800

    :tada: Create empty project

---
+++ app/src/main/java/com/leomarkpaway/todoapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.leomarkpaway.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.leomarkpawaty.todoapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
